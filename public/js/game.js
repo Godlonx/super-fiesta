e089503d28259f1f93e6a109d0c78eb8a986cd05
Godlonx <godlonx44@gmail.com>
2023-04-12 16:12:06 +0200

    :construction: | Working on game window

--- public/js/game.js
+++ public/js/game.js
@@ -1,25 +1,40 @@
+// import {Board} from './chess.js'
+
 const board = document.createElement("div");
 board.className = "board";
 document.body.appendChild(board);
 
 for (let i = 0; i < 8; i++) {
-  for (let c = 0; c < 8; c++) {
-    const cell = document.createElement("div");
-    if (i%2 == 0) {
-      if (c % 2 == 0) {
-        cell.className = "cell white"
-      } else {
-        cell.className = "cell black"
-      }
-    } else {
-      if (c % 2 != 0) {
-        cell.className = "cell white"
-      } else {
-        cell.className = "cell black"
-      }
+    for (let c = 0; c < 8; c++) {
+        const cell = document.createElement("div");
+        if (i%2 == 0) {
+            if (c % 2 == 0) {
+                cell.className = "cell white"
+            } else {
+                cell.className = "cell black"
+            }
+        } else {
+            if (c % 2 != 0) {
+                cell.className = "cell white"
+            } else {
+                cell.className = "cell black"
+            }
+        }
+        cell.id = c+8*i
+        cell.onclick = function(){ GetPiece(cell.id) }
+        board.appendChild(cell)
     }
-    cell.id = c+8*i
-    cell.onclick = function(){ console.log(cell.id)}
-    board.appendChild(cell)
-  }
+}
+let handedPiece
+let callIndex
+const GetPiece = (cellPos) => {
+    let val = document.getElementById(cellPos)
+    if (handedPiece) {
+        val.innerHTML = handedPiece
+        callIndex = null
+    }
+    if (val.innerHTML != null) {
+        handedPiece = val.innerHTML
+        callIndex = cellPos 
+    }
 }
