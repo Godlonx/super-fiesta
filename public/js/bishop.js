bff6cb017bf7c710317cebfc866cd060e21e6f44
Godlonx <godlonx44@gmail.com>
2023-04-14 16:21:15 +0200

    :bug: | Fix bishop preMoves error

--- public/js/bishop.js
+++ public/js/bishop.js
@@ -9,18 +9,19 @@ export default class Bishop extends Piece {
     checkMovementsRight() {
         const posX = Math.trunc(this._pos/8)
         const posY = this._pos%8
+        console.log(posX, posY, 8-posX, 8-posY);
         for (let i=1; i<8; i++) {
-            console.log(posX, posY);
-            if (i<=Math.min(posY, 8-posX)) { // Left-Down
+            console.log(i, 7-posY, posX);
+            if (i<=Math.min(posY, 8-posX-1)) { // Left-Down
                 this._possibleMoves.push(this._pos+(7*i))
             }
             if (i<=Math.min(posY, posX)) { // Left-Up
                 this._possibleMoves.push(this._pos-(9*i))
             }
-            if (i<Math.min(8-posY, posX)) { // Right-Up
+            if (i<=Math.min(8-posY-1, posX)) { // Right-Up
                 this._possibleMoves.push(this._pos-(7*i))
             }
-            if (i<Math.min(8-posY, 8-posX)) { // Right-Down
+            if (i<=Math.min(8-posY-1, 8-posX-1)) { // Right-Down
                 this._possibleMoves.push(this._pos+(9*i))
             }
         }
