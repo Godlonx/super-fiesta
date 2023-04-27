d34cc8dca81f756d76748b1163dbe49f6205db27
lrichard <Rayce16@laposte.net>
2023-04-27 14:13:17 +0200

    :white_check_mark: | add upgrade.js

---
+++ public/js/upgrade.js
@@ -0,0 +1,8 @@
+const piecesUpgrade = {
+    "lvl": {
+        "0":{"pieces":["pawnX3", "knight"]},
+        "1":{"pieces":["pawnX3", "bishop"]},
+        "2":{"pieces":["knightX2", "bishopX2", "rook"]},
+        "3":{"pieces":["knightX3", "bishopX3", "rookX2", "queen"]}
+    }
+}
